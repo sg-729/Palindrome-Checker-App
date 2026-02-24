df2bddbe2330ecad8bc72c5590ed2ef874c220cf
sg-729 <sg9906@srmist.edu.in>
2026-02-24 16:44:19 +0530

    UC3:String Reverse

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,17 +1,19 @@
 public class PalindromeCheckerApp {
     public static void main (String args[]){
-        //UC2
+        //UC3
         String input = "madam";
-        boolean isPalindrome = true;
+        String reversed = "";
 
-        for (int i = 0; i < input.length() / 2; i++) {
-            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
-                isPalindrome = false;
-                break;
-            }
+        // Iterate from the last character to the first
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed = reversed + input.charAt(i);
         }
 
+        boolean isPalindrome = input.equals(reversed);
+
         System.out.println("Input text: " + input);
+        System.out.println("Reversed text: " + reversed);
         System.out.println("Is it a Palindrome? : " + isPalindrome);
     }
 }
+
