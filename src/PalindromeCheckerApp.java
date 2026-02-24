345558c96feca7d4163505bf497ac62c410b2689
sg-729 <sg9906@srmist.edu.in>
2026-02-24 16:15:59 +0530

    UC1: Add welcome message module

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,2 +1,8 @@
 public class PalindromeCheckerApp {
+    public static void main (String args[]){
+        System.out.println("Welcome to the Palindrome Checker Management system");
+        System.out.println("Version : 1.0 ");
+        System.out.println("System initiated successfully");
+    }
+
 }
